a0fbac7cef05af9f7108b7438f3ad311a4b27965
Diego Buencuero <diegobuencuero4095@gmail.com>
2026-04-11 19:16:00 -0300

    alowe

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#slkw6b*t!0im%(ohw+@&o7u!*18w%6l654-9rdaln@y_j_6on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "https://innobreed.pythonanywhere.com", "127.0.0.1" ]
+ALLOWED_HOSTS = [ 'innobreed.pythonanywhere.com', '127.0.0.1' ]
     
 
 # Application definition
